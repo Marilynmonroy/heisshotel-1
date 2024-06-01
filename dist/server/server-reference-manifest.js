5aa6cd87c5ddd55f45442371c54392596a92873f
Marilyn <mararago@publicisgroupe.net>
2024-06-01 16:09:50 -0500

    .

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"COtEHI2QkhoVj51cTeX6CV/gIFZ15LSD/nAh/zfwDvQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"rqQCn60a54cCO/NC4P7LWifcJN9g8hEYZnitSZxBCEk=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "dist",
+  trailingSlash: true,
 };
 
 export default nextConfig;
