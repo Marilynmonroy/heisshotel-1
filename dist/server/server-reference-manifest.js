ea40ea8045dc7f2b0964d51e8f9c20e6f48091c0
Marilyn <mararago@publicisgroupe.net>
2024-06-01 15:13:07 -0500

    vercel

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ELVvQFAHKxp8gKy3iPZz3F2w1YQEF9xSeFdkIotdXrk=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"H/jVvBobiEMX/zcnWJeO+Ug8pKFSVxAGgqFDbcaSCvg=\"\n}"
