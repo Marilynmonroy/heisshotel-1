3cc3e65945965f71a11172f3bff26610b3c1f60d
Marilyn <mararago@publicisgroupe.net>
2024-06-27 21:12:52 -0500

    w-env

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    CONTENTFUL_SPACE_ID: "h7d6zn9m22m3",
-    CONTENTFUL_ACCESS_TOKEN: "gTmKyQeinfmz-iWrVAjL-RQ73WflCqOgsTHC76NvkLc",
-
-    ENTRY_ID_HOME_HABITACIONES: "5UTaxxYz6ietNRev9Wabrm",
-    ENTRY_ID_HOME_ESPACIOS: "2l69nK6Z81ZIFK6klcNXl2",
-    ENTRY_ID_HOME_EXPERIENCIAS: "3Y2p8CIbJcXIYpFRGhCKK5",
-    ENTRY_ID_HOME: "7gX1IYINCMrSD6cSWtZPBX",
-  },
 };
 
 export default nextConfig;
